74dab388eeb4faa2602becddf443a23546e6c7e7
annazhong86 <annazhong86@gmail.com>
2018-04-11 22:03:35 +0800

    105 Footer

--- app.js
+++ app.js
@@ -1,75 +1,85 @@
 angular.module('app', [
-    'ui.grid'
-]).controller('MainCtrl', MainCtrl);
+    'ui.grid',
+    // selected rows will be shown if using ui-grid-selectioon featue.
+    'ui.grid.selection'
+])
+.controller('MainCtrl', MainCtrl)
+.filter('mapGender', mapGender);
 
 MainCtrl.$inject = ['$http', 'uiGridConstants', 'myData'];
 
 function MainCtrl($http, uiGridConstants, myData) {
     var vm = this;
+    myData.forEach(function(row){
+        row.registered = Date.parse(row.registered);
+        row.gender = row.gender === 'male' ? '1' : '2';
+    })
+    
+    vm.highlightFilteredHeader = function( row, rowRenderIndex, col, colRenderIndex ) {
+        if( col.filters[0].term ){
+          return 'header-filtered';
+        } else {
+          return '';
+        }
+      };
+
     vm.gridOptions = {
+        //Display total rows and the number of filtered rows
+        showGridFooter: true,
+        showColumnFooter: true,
+        enableFiltering: true,
         enableSorting:true,
         data:myData,
         columnDefs:[
             {
-                field:'firstName',
-                sort: {
-                    direction: uiGridConstants.DESC,
-                    priority: 1
-                }
+                field:'name', width:'10%'
             },
-            {
-                field:'lastName', 
-                enableSorting: false
+            { 
+                field: 'gender', 
+                filter: {
+                term: '1',
+                type: uiGridConstants.filter.SELECT,
+                selectOptions: [ { value: '1', label: 'male' }, { value: '2', label: 'female' }, { value: '3', label: 'unknown'}, { value: '4', label: 'not stated' }, { value: '5', label: 'a really long value that extends things' } ]
+              },
+              cellFilter: 'mapGender', 
+              headerCellClass: vm.highlightFilteredHeader,
+              width:'10%' 
             },
-            {
-                field:'company',
-                sort: {
-                    direction: uiGridConstants.ASC,
-                    priority: 0
-                },
-                suppressRemoveSort: true,
-                sortingAlgorithm: function(a,b,rowA,rowB,direction){
-                    //console.log(a,b,rowA,rowB,direction)
-                    var nulls = vm.gridApi.core.sortHandleNulls(a,b);
-                    if(nulls !== null) {
-                        return nulls;
-                    } else {
-                        if( a === b){
-                            return 0;
-                        }
-                        if( a === true){
-                            return 1;
-                        }
-                        if(b === false){
-                            return -1;
-                        }
-                        if( a === false){
-                            return 1;
-                        }
-                        if(b === true){
-                            return -1;
-                        }
-                        return 0;
-                    }
-                }
-            },{
-                field:'employed',
-                sort: {
-                    direction: uiGridConstants.DESC,
-                    priority: 2
-                }
-            }
-
+            {field:'address.street', aggregationType: uiGridConstants.aggregationTypes.sum, width:'10%'},
+            {field:'age', aggregationType: uiGridConstants.aggregationTypes.avg, aggregationHideLabel:true, width:'10%'},
+            {field:'age', name:'ageMin', aggregationType: uiGridConstants.aggregationTypes.min, width:'12%', displayName: 'Age for min'},
+            {field:'age', name:'ageMax', aggregationType: uiGridConstants.aggregationTypes.max, width:'13%', displayName:'Age for max'},
+            {field:'age', name:'customColumnFooterTemplate', width:'14%', footerCellTemplate:'<div class="ui-grid-contents" style="background-color:red; color: White">custom template</div>'},
+            {name:'registered', field:'registered', width:'20%', cellFilter:'date', footerCellFilter:'date', aggregationType: uiGridConstants.aggregationTypes.max}            
         ],
         onRegisterApi: function(gridApi){
             vm.gridApi = gridApi;
         }
     }
-    vm.toggleEmployed = function(){
-        if(vm.gridOptions.data[2].employed === false){
-            vm.gridOptions.data[2].employed = true;
-        };
-        console.log(vm.gridOptions.data[2])
-        vm.gridApi.core.notifyDataChange(uiGridConstants.dataChange.EDIT);
+
+    vm.toggleGridFooter = function(){
+        $scope.gridOptions.showGridFooter = !$scope.gridOptions.showGridFooter;
+        $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.OPTIONS);
     }
+
+    vm.toggleColumnFooter = function(){
+        $scope.gridOptions.showColumnFooter = !$scope.gridOptions.showColumnFooter;
+        $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.OPTIONS);
+    }
+    
 }
+
+function mapGender() {
+    var genderHash = {
+      1: 'male',
+      2: 'female'
+    };
+   
+    return function(input) {
+      if (!input){
+        return '';
+      } else {
+        return genderHash[input];
+      }
+    };
+  }
